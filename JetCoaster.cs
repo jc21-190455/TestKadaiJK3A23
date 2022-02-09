c5da40d32131e23743cfecb7c2c25fb3985db4a7
遥香 鈴木 <190455@jc-21.jp>
2022-02-09 17:19:29 +0900

    jetcoasterテスト成功

---
+++ JetCoaster.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yosi12l10.Properties
+{
+    public class JetCoaster
+    {
+        //k:定員、g:配列、ｒ:回す回数
+
+
+
+        static int i = 0;
+
+
+
+        public static int ride(int k, int[] g, int r)//メイン
+        {
+            i = 0;
+            int saisyukekka = 0;
+
+            for (int j = 1; j <= r; j++)//回す回数
+            {
+                saisyukekka += ride2(k, g);
+            }
+            return saisyukekka;
+
+
+
+
+        }
+
+
+
+
+        public static int ride2(int k, int[] g)//定員、配列
+        {
+            int kekka = 0;
+
+
+
+            Boolean flg = true;
+            while (flg)
+            {
+                Console.Write(i);
+                if (kekka + g[i] > k)
+                {
+                    flg = false;
+
+                }
+                else
+                {
+                    kekka += g[i];
+                    i = (i + 1) % g.Length;
+                }
+
+            }
+
+
+
+            return kekka;
+
+        }
+
+
+
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yosi12l10
+{
+    public class kuriage
+    {
+        //おそらくOK
+        public static int kuriage10(int i)
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            else if (i >= 1)
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Ceiling(j / 10) * 10);
+                return i;
+            }
+            else
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Floor(j / 10) * 10);
+                return 0;
+            }
+
+        }
+
+    }
+
+}
